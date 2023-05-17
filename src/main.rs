8d167c9134e14cff36001d042732989561005e36
Léana 江 <leana.jiang@icloud.com>
2023-05-17 17:49:00 +0200

    add: telnet_parser

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,44 @@
-fn main() {
-    println!("Hello, world!");
+mod telnet_parser;
+use telnet_parser::TelnetParser;
+
+use tokio::{
+    io::{AsyncReadExt, AsyncWriteExt},
+    net::TcpListener,
+};
+
+#[tokio::main]
+async fn main() -> Result<(), anyhow::Error> {
+    let listener = TcpListener::bind("127.0.0.1:23").await?;
+
+    loop {
+        let (mut socket, _) = listener.accept().await?;
+
+        tokio::spawn(async move {
+            let mut parser = TelnetParser::new();
+            let mut buf = [0; 1024];
+            while !parser.exit_now() {
+                parser.clear();
+
+                match socket.read(&mut buf).await {
+                    // HACK: remove the unwraps
+                    Ok(n) => {
+                        parser.read_codes(&buf[..n]);
+                        socket.write(parser.respond()).await.unwrap();
+                        socket
+                            .write(
+                                format!("width: {} height: {}\n", parser.width(), parser.height())
+                                    .as_bytes(),
+                            )
+                            .await
+                            .unwrap();
+                    }
+                    Err(_) => (),
+                };
+
+                if parser.exit_now() {
+                    socket.write(b"Byeeeee #Clawthorn #TOH\n").await.unwrap();
+                };
+            }
+        });
+    }
 }
---
+++ src/telnet_parser.rs
@@ -0,0 +1,103 @@
+use tokio::{
+    io::{AsyncReadExt, AsyncWriteExt},
+    net::TcpListener,
+};
+
+// Telnet protocol characters
+const IAC: u8 = 255; // "Interpret As Command"
+const DONT: u8 = 254;
+const DO: u8 = 253;
+const WONT: u8 = 252;
+const WILL: u8 = 251;
+const NULL: u8 = 0;
+
+const SB: u8 = 250; // Subnegotiation Begin;
+const SE: u8 = 240; // Subnegotiation End;
+const NOP: u8 = 241; // No Operation;
+const DM: u8 = 242; // Data Mark;
+const BRK: u8 = 243; // Break;
+const IP: u8 = 244; // Interrupt process;
+const AO: u8 = 245; // Abort output;
+const AYT: u8 = 246; // Are You There;
+const EC: u8 = 247; // Erase Character;
+const EL: u8 = 248; // Erase Line;
+const GA: u8 = 249; // Go Ahead;
+
+// telnet options
+const NAWS: u8 = 31;
+const TM: u8 = 6;
+
+#[derive(Default)]
+pub struct TelnetParser {
+    width: u8,
+    height: u8,
+    response: Vec<u8>,
+    exit_now: bool,
+}
+
+impl TelnetParser {
+    pub fn new() -> Self {
+        TelnetParser {
+            response: Vec::with_capacity(1024),
+            ..Default::default()
+        }
+    }
+
+    pub fn clear(&mut self) {
+        self.response.clear();
+    }
+
+    pub fn read_codes(&mut self, codes: &[u8]) {
+        // use Commands::*;
+        match codes {
+            [] => (),
+
+            // Handle terminal size
+            [IAC, WILL, NAWS, rest @ ..] => {
+                self.response.append(&mut vec![IAC, DO, NAWS]);
+                self.read_codes(rest);
+            }
+            [IAC, SB, NAWS, 0, width, 0, height, IAC, SE, rest @ ..] => {
+                self.width = *width;
+                self.height = *height;
+                self.read_codes(rest);
+            }
+
+            // Override the ^C behaviour
+            [IAC, IP, IAC, DO, TM, rest @ ..] => {
+                self.exit_now = true;
+                self.response.append(&mut vec![IAC, IP, IAC, WONT, TM]);
+                self.read_codes(rest);
+            }
+
+            [IAC, DO, option, rest @ ..] => {
+                self.response.append(&mut vec![IAC, WONT, *option]);
+                self.read_codes(rest);
+            }
+            [IAC, WILL, option, rest @ ..] => {
+                self.response.append(&mut vec![IAC, WONT, *option]);
+                self.read_codes(rest);
+            }
+            _ => {
+                eprintln!("Unimplemented sequence: {:?}", codes);
+            }
+        }
+    }
+
+    pub fn respond(&self) -> &[u8] {
+        &self.response[..]
+    }
+
+    pub fn exit_now(&self) -> bool {
+        self.exit_now
+    }
+
+    pub fn height(&self) -> u8 {
+        self.height
+    }
+
+    pub fn width(&self) -> u8 {
+        self.width
+    }
+}
+
